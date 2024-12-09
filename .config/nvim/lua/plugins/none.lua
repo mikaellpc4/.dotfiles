bdc09874fa8757c09226da58e1d68f50cb78bcb1
mikaellpc4 <mikaellpc4@gmail.com>
2024-12-09 14:59:35 -0300

    seila

--- .config/nvim/lua/plugins/none.lua
+++ .config/nvim/lua/plugins/none.lua
@@ -6,8 +6,8 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.diagnostics.phpstan,
-				null_ls.builtins.formatting.pint,
+				-- null_ls.builtins.diagnostics.phpstan,
+				-- null_ls.builtins.formatting.pint,
 			},
 		})
 	end,
